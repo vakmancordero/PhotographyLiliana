c222226b6d2a6470b934dba432ed206d9a99df65
Jesús Fco Cortés <jfcr@live.com>
2017-04-16 00:16:50 -0500

    migration from curriculum maked

--- app/Http/Controllers/CurriculumController.php
+++ app/Http/Controllers/CurriculumController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Curriculum;
 
 class CurriculumController extends Controller
 {
-    //
+    public function index() {
+        return view('admin/curriculum/index');
+    }
 }
