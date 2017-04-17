32c55fd8c1c26b577e5a0617bbf55633490c3860
Arturo Cordero <vakmancordero@gmail.com>
2017-04-17 02:24:44 -0500

    CurriculumImage Model [Methods]

--- app/CurriculumImage.php
+++ app/CurriculumImage.php
@@ -2,7 +2,9 @@
 
 namespace App;
 
+use App\Curriculum;
 use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class CurriculumImage extends Model {
 	
@@ -11,5 +13,43 @@ class CurriculumImage extends Model {
 	protected $fillable = [
 		'curriculum_id', 'name', 'path', 'order'
 	];
+	
+	public $timestamps = false;
+	
+	protected $baseDir = 'images/articles';
+	
+	public function article() {
+		return $this->belongsTo('App\Curriculum');
+	}
+	
+	public static function build($name) {
+		
+		$image = new self;
+		
+		return $image->saveAs($name);
+	}
+	
+	private function saveAs($name) {
+		
+		$this->name = sprintf("%s-%s", time(), $name);
+		$this->path = sprintf("%s/%s", $this->baseDir, $this->name);
+		
+		return $this;
+	}
+	
+	public function store(UploadedFile $file) {
+		
+		$file->move($this->baseDir, $this->name);
+		
+		return $this;
+	}
+	
+	public function delete() {
+		
+		File::delete([
+			$this->path,
+		]);
+		parent::delete();
+	}
     
 }
