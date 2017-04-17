720df93a2bac54cc0bb5e94f5abb4fdbf4618cc5
Arturo Cordero <vakmancordero@gmail.com>
2017-04-16 21:04:50 -0500

    Migrations [Article & Article_Images]

---
+++ database/migrations/2017_01_01_233104_create_article_images_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticleImagesTable extends Migration
+{
+    /**
+     * Runs the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article_images', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('article_id')->unsigned();
+            $table->string('name');
+            $table->string('path');
+            $table->timestamps();
+            $table->foreign('article_id')
+                ->references('id')
+                ->on('articles')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article_images');
+    }
+}
