fefcc01e5f43d73b7ddde3cbe3a25e50991c8ad4
Jesús Fco Cortés <jfcr@live.com>
2017-04-17 04:28:29 -0500

    upload vista mejorada

--- resources/views/admin/curriculum/upload.blade.php
+++ resources/views/admin/curriculum/upload.blade.php
@@ -7,25 +7,53 @@
 
 @section('stylesheets')
     <link rel="stylesheet" type="text/css" href="{{ asset('dropzone/dropzone.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/appAdmin/photosFacebook.css') }}">
 @stop
 
 @section('content')
 
-    <h1>Panel de control - Currículums</h1>
+    <h1>Currículums - {{ $curriculum->name }}</h1>
     <h3>Añada sus imágenes</h3>
 
     <div class="row">
         <div class="col-md-4">
-            <h1>{{ $curriculum->name }}</h1>
-            <hr>
             <div class="description">
                 {!! nl2br($curriculum->description) !!}
             </div>
         </div>
+    </div>
+
+        <form id="addImagesForm"
+              action="{{ route('store_path', [$curriculum->id]) }}"
+              method="POST"
+              class="dropzone">
+            {{ csrf_field() }}
+        </form>
+<br>
+
+            <div id="photoContainer">
+                @foreach ($curriculum->curriculumImages as $image)
+
+                        <div id="photos" style="background-image: url(/{{ $image->path }}" data-lity>
+                            <div id="photoHover">
+                                <form class="deleteImageForm" action="/admin/curriculum/images/{{ $image->id }}">
+                                    {!! csrf_field() !!}
+                                    <input type="hidden" name="_method" value="DELETE">
+                                    <button type="submit" class="ui icon button"><i class="trash icon"></i></button>
+                                </form>
+
+                            </div>
+                        </div>
+                    
+                @endforeach
+            </div>
+
+    <br>
+    <div class="row">
         <div class="col-md-8 gallery">
             @foreach ($curriculum->curriculumImages->chunk(4) as $set)
-                <div class="row">
-                    @foreach ($set as $image)
+
+                    @foreach ($curriculum->curriculumImages as $image)
                         <div class="col-md-3">
                             <a href="/{{ $image->path }}" data-lity>
                                 <img class="img-fluid img-thumbnail"
@@ -39,23 +67,20 @@
                             </form>
                         </div>
                     @endforeach
-                </div>
+
             @endforeach
             {{--@if(!Auth::guest())--}}
                 {{--@if ($article->ownedBy(Auth::user()))--}}
                     <hr>
 
-                    <h2>Add Your Images</h2>
 
-                    <form id="addImagesForm"
-                          action="{{ route('store_path', [$curriculum->id]) }}"
-                          method="POST"
-                          class="dropzone">
-                        {{ csrf_field() }}
-                    </form>
                 {{--@endif--}}
             {{--@endif--}}
         </div>
     </div>
 
 @endsection
+
+@section('script')
+    <script src="{{url('js/appAdmin/photosFacebook.js')}}"></script>
+@stop
